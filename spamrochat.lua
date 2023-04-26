4a19a7359752a39f9a0381fd112ee413efa6cef1
Hiro? <brenofprocopio@gmail.com>
2023-04-25 22:35:27 -0300

    useless

--- spamrochat.lua
+++ spamrochat.lua
@@ -1,10 +1,7 @@
-local num = 0
 while true do
     if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Y) then
         break
     end
 	game:GetService("ReplicatedStorage"):WaitForChild("HDAdminClient"):WaitForChild("Signals"):WaitForChild("RequestCommand"):InvokeServer(";r6 ;hat me 33070696")
     wait(1.5)
-    num = num + 1
-    print("LOOPS " .. num)
 end
